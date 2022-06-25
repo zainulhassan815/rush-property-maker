70b7dd7c8f59880d9a57a14aeb2b3d7aaf550678
GitHub Action <action@github.com>
2022-06-25 07:58:08 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/zainulhassan815/rushpropertymaker/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.zainulhassan815.rushpropertymaker.services
+
+import com.github.zainulhassan815.rushpropertymaker.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
